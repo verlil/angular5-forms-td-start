c8233d5a6f1650a068a3fe36eeed11ffe36af364
verlil <vernigoralilia@gmail.com>
2018-06-18 05:26:31 +0300

    override one field, that is wrapped by ngForm itself, programmatically using this.signupForm.form.patchValue({...});

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,19 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    this.signupForm.setValue({
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'dafault@mail.com'
+    //   },
+    //   questionAnswer: 'none',
+    //   secret: 'teacher',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
       userData: {
-        username: suggestedName,
-        email: 'dafault@mail.com'
-      },
-      questionAnswer: 'none',
-      secret: 'teacher',
-      gender: 'male'
+        username: suggestedName
+      }
     });
   }
 
